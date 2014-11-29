8894ff4eba017e261eebac8f2d8f97b94a522f25
shimurausk <shimurausk@gmail.com>
2014-11-30 01:48:32 +0900

    comment_complete

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,30 @@
 class Blog < ActiveRecord::Base
-	#has_many :comments,dependent: :destroy
+	has_many :comments,dependent: :destroy
 	mount_uploader :avatar, AvatarUploader
-	# don't forget those if you use :attr_accessible (delete method and form caching method are provided by Carrierwave and used by RailsAdmin)
-  #attr_accessible :avatar, :avatar_cache, :remove_avatar
-  #attr_accessible :title, :text, :avatar, :tag_list
-
-
 	validates :title, presence: true,
-					  length: { minimum: 6 }
+				  length: { minimum: 6 }
+  validates :category, presence: true
+  has_many :taggings
+  has_many :tags,through: :taggings
+
+  def self.tagged_with(name)
+  	Tag.find_by_name!(name).blogs
+  end
+
+  def self.tag_counts
+    #binding.pry
+  	Tag.select("tags.*,count(taggings.tag_id) as count").
+  	joins(:taggings).group("taggings.tag_id")
+  end
+
+  def tag_list
+  	tags.map(&:name).join(", ")
+  end
+
+  def tag_list=(names)
+  	self.tags = names.split(",").map do |n|
+  		Tag.where(name: n.strip).first_or_create!
+  	end	
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117145046) do
+ActiveRecord::Schema.define(version: 20141129153331) do
 
   create_table "blogs", force: true do |t|
     t.string   "title"
@@ -23,6 +23,31 @@ ActiveRecord::Schema.define(version: 20141117145046) do
     t.string   "category"
   end
 
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "blog_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["blog_id"], name: "index_comments_on_blog_id"
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "blog_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["blog_id"], name: "index_taggings_on_blog_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
